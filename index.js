099abaf5aa71365952e78671b741b2a2b2745036
RohanJohnson <120460285+RohanJohnson@users.noreply.github.com>
2023-02-17 12:05:56 +0000

    initial commit
    working json relational connection

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+const songData = require('./api/slotify.json')
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+
+app.get('/', (req, res) => {
+    res.render(`home`)
+})
+
+
+
+app.get('/s/:songId', (req, res) => {
+    const { songId } = req.params;
+    const data = songData[2].data[songId]
+    const artists = songData[3].data[1]
+
+
+    if (data) {
+        res.render('song', { ...data , ...artists});
+    } else {
+        res.render('notfound', { songId })
+    }
+})
+
+app.listen(3001, () => {
+    console.log('running');
+})
