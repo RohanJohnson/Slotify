bd5fa47af210208a5154cdc1a7f2930c9b72e49f
RohanJohnson <120460285+RohanJohnson@users.noreply.github.com>
2023-02-17 13:53:10 +0000

    3 files modified

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const path = require('path');
-const songData = require('./api/slotify.json')
+const songData = require('./api/slotify.json');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'))
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
-    res.render(`home`)
-})
-
-
+  songs = songData[2].data;
+  res.render(`home`, songs);
+});
 
 app.get('/s/:songId', (req, res) => {
-    const { songId } = req.params;
-    const data = songData[2].data[songId]
-    const artists = songData[3].data[data.artist-1]
+  const { songId } = req.params;
+  const data = songData[2].data[songId];
+  const artists = songData[3].data[data.artist - 1];
 
-
-    if (data) {
-        res.render('song', { ...data , ...artists});
-    } else {
-        res.render('notfound', { songId })
-    }
-})
+  if (data) {
+    res.render('song', { ...data, ...artists });
+  } else {
+    res.render('notfound', { songId });
+  }
+});
 
 app.listen(3001, () => {
-    console.log('running');
-})
+  console.log('running');
+});
